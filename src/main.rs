bf68b39cc15e0ffe2919c97f1ec6a1088b653332
Syfaro <syfaro@huefox.com>
2023-07-06 14:44:51 -0400

    Listen on all hosts.

--- src/main.rs
+++ src/main.rs
@@ -145,7 +145,7 @@ async fn main() -> eyre::Result<()> {
         )
         .with_state(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
